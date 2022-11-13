3a922d6febd0be15338e83c9c95a5548e12c7fbd
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-11-13 18:09:55 +0100

    iteration 1, add prettier, add tailwinds

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,20 @@
 // Iteration #1
+const { Schema, model } = require('mongoose');
+
+const droneSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    propellers: Number,
+    maxSpeed: Number,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Drone = model('Drone', droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model');
+
+require('dotenv/config');
+const mongouri =
+  process.env.MONGODB_URI || 'mongodb://localhost/lab-express-drones';
+mongoose.connect(mongouri);
+
+const drones = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 },
+];
+
+Drone.insertMany(drones)
+  .then((dronesFromSeed) => {
+    console.log(`Success - ${dronesFromSeed.length} drones got created`);
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log(err));
