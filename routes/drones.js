14c7156816f50e7ea1a108f6c91c567f3aa7bf34
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-11-14 11:45:40 +0100

    iteration 4

--- routes/drones.js
+++ routes/drones.js
@@ -5,18 +5,6 @@ const router = express.Router();
 
 const Drone = require('../models/Drone.model');
 
-// Promise
-// router.get('/drones', (req, res, next) => {
-//   Drone.find()
-//     .then((list) => {
-//       res.render('drones/list', list);
-//     })
-//     .catch((err) => {
-//       next(err);
-//       console.log(err);
-//     });
-// }),
-
 router.get('/drones', async (req, res, next) => {
   try {
     const list = await Drone.find();
@@ -48,14 +36,32 @@ router.post('/drones/create', async (req, res, next) => {
   }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.get('/drones/:id/edit', async (req, res, next) => {
+  const droneId = req.params.id;
+  try {
+    const drone = await Drone.findById(droneId);
+    console.log(drone);
+    res.render('drones/update-form', drone);
+  } catch (err) {
+    console.log(err.message);
+    res.redirect('/');
+  }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+router.post('/drones/:id/edit', async (req, res, next) => {
+  const droneId = req.params.id;
+  const { name, propellers, maxSpeed } = req.body;
+  try {
+    await Drone.findByIdAndUpdate(droneId, {
+      name,
+      propellers,
+      maxSpeed,
+    });
+    res.redirect('/drones');
+  } catch (err) {
+    console.log(err.message);
+    res.redirect('/');
+  }
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
@@ -64,3 +70,15 @@ router.post('/drones/:id/delete', (req, res, next) => {
 });
 
 module.exports = router;
+
+// Promise
+// router.get('/drones', (req, res, next) => {
+//   Drone.find()
+//     .then((list) => {
+//       res.render('drones/list', list);
+//     })
+//     .catch((err) => {
+//       next(err);
+//       console.log(err);
+//     });
+// }),
